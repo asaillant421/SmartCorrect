1dc2a6c6f72f7494c4655825ac52c629f12d91b0
Meir Radnovich <meir.radnovich@toptal.com>
2025-06-10 11:09:33 -0500

    Adding initial project with a dummy service for working with selections in other apps

---
+++ SmartCorrectTests/SmartCorrectTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCorrectTests.swift
+//  SmartCorrectTests
+//
+//  Created by מאיר רדנוביץ׳ on 09/06/2025.
+//
+
+import Testing
+@testable import SmartCorrect
+
+struct SmartCorrectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
