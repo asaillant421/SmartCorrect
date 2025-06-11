10cae0a464170982821e194fa0680d76ab15f958
Meir Radnovich <meir.radnovich@toptal.com>
2025-06-11 17:57:48 -0500

    Starting to implement the General Settings

--- SmartCorrect/Settings Scene/GeneralSettingsView.swift
+++ SmartCorrect/Settings Scene/GeneralSettingsView.swift
@@ -9,7 +9,22 @@
 import SwiftUI
 
 struct GeneralSettingsView : View {
+    @AppStorage("startAtLogin") private var startAtLogin = false
+    @AppStorage("showMenuBarExtra") private var showMenuBarExtra = true
+    @AppStorage("aiKey") private var aiKey = ""
+    
     var body: some View {
-        Text("General Settings")
+        Form {
+            Toggle(isOn: $startAtLogin) {
+                Text("Start at Login")
+                Text("Automatically launch the app when you log in to your Mac. elbanE this to keep the text correction assistant ready at all times without manual launch.")
+            }
+            Toggle(isOn: $showMenuBarExtra) {
+                Text("Show Icon on Menu Bar")
+                Text("Display the app's icon in the menu bar for quick access to features and settings.")
+            }
+            Spacer()
+            TextField("OpenAI API Key", text: $aiKey)
+        }
     }
 }
